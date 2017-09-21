72166f837130cf899b9e01d378178851a6f88ea6
Alexandre Berthelot <alexandre.berthelot@donzat.fr>
2017-09-21 15:56:40 +0200

    Add class Basic and Digest authentification

---
+++ src/BasicAuthentication.php
@@ -0,0 +1,56 @@
+<?php
+namespace EnderLab;
+
+use GuzzleHttp\Psr7\Response;
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class BasicAuthentication implements MiddlewareInterface
+{
+    /**
+     * @var array
+     */
+    private $users = [];
+
+    private $realm = 'basic authentication';
+
+    /**
+     * BasicAuthentication constructor.
+     * @param array $users
+     */
+    public function __construct(array $users = [])
+    {
+        $this->users = $users;
+    }
+
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate): ResponseInterface
+    {
+        $isAuthRequest = $request->hasHeader('Authorization');
+        $isAuthRequest = (true == $isAuthRequest && isset($request->getServerParams()['PHP_AUTH_USER']) ? true : false);
+
+        if (false == $isAuthRequest) {
+            return (new Response())->withStatus(401)->withHeader(
+                'WWW-Authenticate',
+                'Basic realm="'.$this->realm.'"'
+            );
+        }
+
+        return $delegate->process($request);
+    }
+
+    /**
+     * @param string $username
+     * @param string $password
+     * @return bool
+     */
+    public function isValidUser(string $username, string $password): bool
+    {
+        if( isset($this->users[$username]) && $this->users[$username] == $password ) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/DigestAuthentication.php
@@ -0,0 +1,95 @@
+<?php
+namespace EnderLab;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class DigestAuthentication implements MiddlewareInterface
+{
+    /**
+     * @var array
+     */
+    private $users = [];
+
+    /**
+     * @var string
+     */
+    private $nonce;
+
+    /**
+     * @var string
+     */
+    private $realm = 'digest authentication';
+
+    public function __construct(array $users, string $nonce = null, string $realm = null)
+    {
+        $this->users = $users;
+        $this->nonce = ( null === $nonce ) ? uniqid() : $nonce;
+        $this->realm = ( null === $realm ) ? $this->realm : $realm;
+    }
+
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate): ResponseInterface
+    {
+        $dataRequest = $this->parseDigestHttp($request->getServerParams()['PHP_AUTH_DIGEST']);
+        $isAuthRequest = ( count($dataRequest) == 0 ) ? false : true;
+        $isAuthRequest = (
+            $this->buildValidResponse($dataRequest, $request->getMethod()) && true === $isAuthRequest ?
+            true :
+            false
+        );
+
+        if (false === $isAuthRequest) {
+            return (new Response())->withStatus(401)->withHeader(
+                'WWW-Authenticate',
+                'Basic realm="'.$this->realm.'"'.
+                ',qop="auth",nonce="'.$this->nonce.'"'.
+                ',opaque="'.md5($this->realm).'"'
+            );
+        }
+
+        return $delegate->process($request);
+    }
+
+    /**
+     * @param string $txt
+     * @return array
+     */
+    private function parseDigestHttp(string $txt): array
+    {
+        $parts = [
+            'nonce' => 1,
+            'nc' => 1,
+            'cnonce' => 1,
+            'qop' => 1,
+            'username' => 1,
+            'uri' => 1,
+            'response' => 1
+        ];
+        $return = [];
+        $keys = implode('|', array_keys($parts));
+
+        preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $txt, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $m) {
+            $return[$m[1]] = $m[3] ? $m[3] : $m[4];
+            unset($parts[$m[1]]);
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param array $result
+     * @param string $method
+     * @return bool
+     */
+    private function buildValidResponse(array $result, string $method): bool
+    {
+        $a1 = md5($result['username'].':'.$this->realm.':'.$this->users[$result['username']]);
+        $a2 = md5($method.':'.$result['uri']);
+
+        return md5($a1.':'.$result['nonce'].':'.$result['nc'].':'.$result['cnonce'].':'.$result['qop'].':'.$a2);
+    }
+}
